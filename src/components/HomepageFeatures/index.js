69af1d3fd1cd4a783168a70c147445931f5bd27b
aimanalmas <aiman@penateam.com>
2022-11-22 18:11:09 +0500

    third commit

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Who are we?',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Our team consists of consensus protocol researchers from Stanford and experienced layer 1 developers from all over the world.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'What is Babylon?',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Babylon is a new Cosmos project whose vision is to leverage the security of Bitcoin for enhancing the security of Cosmos zones and other PoS chains.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Babylon Technology',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Core primitives for writing onto and reading timestamps on Bitcoin by the consumer zones and protocols that use the timestamp information.
       </>
     ),
   },
