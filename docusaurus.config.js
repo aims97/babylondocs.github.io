6469c816cefe0d980769c3b3529c60b03c584d99
aimanalmas <aiman@penateam.com>
2023-01-03 13:41:52 +0500

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,9 +20,6 @@ const config = {
   projectName: 'babylondocs.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
